470a2ca80d159ae90f5f9eaa46e446b2535ecad2
Fariz Wibisono <frzwibisono@gmail.com>
2025-03-14 05:46:00 +0700

    refactor: rename MyApp to CampingGearRentalApp in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:quiz_provis_c1_2025/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CampingGearRentalApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
